35cab86fc062ff1a0d0ca970df4da98883529604
sjparzival <sjparzival@gmail.com>
2018-04-08 22:47:13 -0500

    apply function example -- to set custom context

--- functions.js
+++ functions.js
@@ -1,7 +1,6 @@
 //executing callback functions
 
 //to sleep after eat
-
 var eat = function() {
   console.log("Done Eating!");	
 }
@@ -12,3 +11,35 @@ var sleep = function(eat) {
 }
 
 sleep(eat);
+
+/* ---- end ------*/
+
+
+//apply and call example -- these functions are used to call a function with a specified context(this) instead of the function's own context(this)
+/* They both are the same except that apply takes an array as argument and call takes as many parameters */
+
+//example1
+function bus() {
+  console.log(this.color);
+}
+
+var car = {
+  color: 'red'
+};
+
+bus.apply(car); //expects bus to print red
+
+//example2
+var object1 = {
+  function1: function() {
+  	console.log(this.color);
+  }
+}
+
+var object2 = {
+  color: "yellow"
+}
+
+object1.function1.apply(object2);
+
+/* ---- end ------*/
